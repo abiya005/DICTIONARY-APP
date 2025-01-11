4f98b0271de394c672b4f72e6f6d5d908cde8235
abiya005 <abiyabiju050805@gmail.com>
2025-01-11 21:41:07 +0530

    js file finalized

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 const input = document.querySelector('input')
 const btn = document.querySelector('button')
-const dictionary = document.querySelector('dictionary-app')
+const dictionaryArea = document.querySelector('.dictionary-app')
 
 //https://api.dictionaryapi.dev/api/v2/entries/en/<word>
 
 async function dictionaryFn(word){
-    const ref = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     .then(res => res.json())
-    console.log(res)
+    return res[0]
 }
+
+
 btn.addEventListener('click', fetchandCreateCard)
+
+
 async function fetchandCreateCard() {
     const data = await dictionaryFn(input.value)
     console.log(data)
-    dictionary.innerHTML = `
+
+    let partOfSpeechArray = []
+    for (let i = 0; i < data.meanings.length; i++)
+        {
+        partOfSpeechArray.push(data.meanings[i].partOfSpeech)
+    }
+    dictionaryArea.innerHTML = `
         <div class="card">
                 <div class="property">
                     <span>Word:</span>
-                    <span>The name of the word</span>
+                    <span>${data.word}</span>
                 </div>
                 <div class="property">
                     <span>Phonetics:</span>
-                    <span>Phonetics will go here</span>
+                    <span>${data.phonetic}</span>
                 </div>
                 <div class="property">
-                    <span>Audio</span>
+                    <span>
+                    <audio controls src="${data.phonetics[0]?.audio || ''}"></audio>
+                    </span>
                 </div>
                 <div class="property">
                     <span>Definition:</span>
-                    <span>Definition will go here</span>
+                    <span>${data.meanings[0].definitions[0].definition}</span>
                 </div>
                 <div class="property">
-                    <span>Parts of speech:</span>
+                    <span>Example</span>
+                    <span>${data.meanings[1]?.definitions[0]?.example || 'No example available'}</span>
+                </div>
+                <div class="property">
+                    <span>${partOfSpeechArray.map(e => e).join(', ')}</span>
                 </div>
 
             </div>
