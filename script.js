775b91772e907e43b48a18db718b9421bdb3af4e
abiya005 <abiyabiju050805@gmail.com>
2025-01-11 21:00:28 +0530

    code transferred from html to js file

--- script.js
+++ script.js
@@ -9,4 +9,31 @@ async function dictionaryFn(word){
     .then(res => res.json())
     console.log(res)
 }
-dictionaryFn('chair')
+btn.addEventListener('click', fetchandCreateCard)
+async function fetchandCreateCard() {
+    const data = await dictionaryFn(input.value)
+    console.log(data)
+    dictionary.innerHTML = `
+        <div class="card">
+                <div class="property">
+                    <span>Word:</span>
+                    <span>The name of the word</span>
+                </div>
+                <div class="property">
+                    <span>Phonetics:</span>
+                    <span>Phonetics will go here</span>
+                </div>
+                <div class="property">
+                    <span>Audio</span>
+                </div>
+                <div class="property">
+                    <span>Definition:</span>
+                    <span>Definition will go here</span>
+                </div>
+                <div class="property">
+                    <span>Parts of speech:</span>
+                </div>
+
+            </div>
+`
+}
